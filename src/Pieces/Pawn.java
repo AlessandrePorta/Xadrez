45e25814c4872b88fc33ee002be381ddec17ae52
Alessandre Porta <ale_junior97@hotmail.com>
2021-05-13 14:39:32 -0300

    Peças setadas manualmente

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -2,7 +2,17 @@ package Pieces;
 
 public class Pawn extends Movimentation {
 
-    private Integer movementPawn;
+    private String movimentPawn = "P";
 
+    public Pawn(String movimentPawn) {
+        this.movimentPawn = movimentPawn;
+    }
 
+    public String getmovimentPawn() {
+        return movimentPawn;
+    }
+
+    public void setmovimentPawn(String movimentPawn) {
+        this.movimentPawn = movimentPawn;
+    }
 }
--- src/Programm/Programm.java
+++ src/Programm/Programm.java
@@ -1,6 +1,8 @@
 package Programm;
 
 import Board.*;
+import Pieces.Pawn;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -23,21 +25,52 @@ public class Programm {
         lista[0][6] = "g";
         lista[0][7] = "h";
 
-        for (int linha = 0; linha <= lista.length - 1; linha++){
-            for (int coluna = 0; coluna < lista.length - 1; coluna++){
-                if (lista[linha][coluna] == null){
+        //Setting - Tower
+        lista[1][0] = "T";
+        lista[8][0] = "T";
+        lista[1][7] = "T";
+        lista[8][7] = "T";
+        //Setting - Horse
+        lista[1][1] = "H";
+        lista[8][1] = "H";
+        lista[1][6] = "H";
+        lista[8][6] = "H";
+        //Setting - Bishop
+        lista[1][2] = "B";
+        lista[8][2] = "B";
+        lista[1][5] = "B";
+        lista[8][5] = "B";
+        //Setting - Queen
+        lista[1][4] = "Q";
+        lista[8][3] = "Q";
+        //Setting - King
+        lista[1][3] = "K";
+        lista[8][4] = "K";
+
+
+        for (int linha = 0; linha <= lista.length - 1; linha++) {
+            for (int coluna = 0; coluna < lista.length - 1; coluna++) {
+                if (lista[linha][coluna] == null) {
                     String aux = "-";
                     lista[linha][coluna] = aux;
                 }
             }
         }
 
-        for (int linha = lista.length-1; linha >= 0; linha--) {
+        for (int n = 0; n < lista.length - 1; n++) {
+            String p = new String("P");
+            lista[2][n] = p;
+            for (int m = 0; m < lista.length - 1; m++){
+                lista[7][m] = p;
+            }
+        }
+
+        for (int linha = lista.length - 1; linha >= 0; linha--) {
             System.out.println();
             int aux = linha;
             column[linha] = aux;
             System.out.print(aux);
-            for (int coluna = 0; coluna <= lista[0].length-1; coluna++) {
+            for (int coluna = 0; coluna <= lista[0].length - 1; coluna++) {
                 System.out.print("\t" + lista[linha][coluna]);
             }
         }
