82c4d8e783d656e5ca436ce9773f05708c9aa3ec
Alessandre Porta <ale_junior97@hotmail.com>
2021-05-13 17:13:08 -0300

    Posição no xadrez por coluna/linha

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package Chess;
 
 import BoardGame.Board;
-import BoardGame.Position;
 import Chess.Pieces.King;
 import Chess.Pieces.Rook;
 
@@ -9,24 +8,28 @@ public class ChessMatch {
 
     private Board board;
 
-    public ChessMatch(){
-        board = new Board(8,8);
+    public ChessMatch() {
+        board = new Board(8, 8);
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces(){
+    public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getLines()][board.getColumns()];
-        for( int i = 0; i < board.getLines(); i++){
-            for (int j = 0; j < board.getColumns(); j++){
+        for (int i = 0; i < board.getLines(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
 
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+    private void placeNewPiece(char column, int line, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, line).toPosition());
+    }
+
+    private void initialSetup() {
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
---
+++ src/Chess/ChessPosition.java
@@ -0,0 +1,37 @@
+package Chess;
+
+import BoardGame.Position;
+
+public class ChessPosition {
+
+    private char column;
+    private int line;
+
+    public ChessPosition(char column, int line) {
+        if (column < 'a' || column > 'h' || line < 1 || line > 8) {
+            throw new ChessException("Erro instanciando posição no xadrez: Válido apenas posições de a1 a h8.");
+        }
+        this.column = column;
+        this.line = line;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - line, column - 'a');
+    }
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getLine());
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + line;
+    }
+}
